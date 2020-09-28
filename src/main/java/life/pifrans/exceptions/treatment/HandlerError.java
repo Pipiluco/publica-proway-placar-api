edb23e0dd51fcc7feee071fc673b3558bfe249a7
Lucas Francisco <lucasf.silva@live.com>
2020-09-28 15:33:12 -0300

    Criação das classes de tratamento de erros HandlerError e StandardError

---
+++ src/main/java/life/pifrans/exceptions/treatment/HandlerError.java
@@ -0,0 +1,27 @@
+package life.pifrans.exceptions.treatment;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import life.pifrans.exceptions.errors.DataIntegrityException;
+import life.pifrans.exceptions.errors.ObjectNotFoundException;
+
+@ControllerAdvice
+public class HandlerError {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException exception, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), exception.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException exception, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+}
