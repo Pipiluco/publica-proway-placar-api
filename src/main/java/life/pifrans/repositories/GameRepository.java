7b9b27de7fea3869f460623da6c5abe8b7933e3f
pifrans <lucasf.silva@live.com>
2020-10-03 21:20:01 -0300

    Adição de novos métodos nas interfaces repositories

--- src/main/java/life/pifrans/repositories/GameRepository.java
+++ src/main/java/life/pifrans/repositories/GameRepository.java
@@ -1,6 +1,10 @@
 package life.pifrans.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import life.pifrans.models.Game;
@@ -8,4 +12,6 @@ import life.pifrans.models.Game;
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 
+	@Query("SELECT g FROM Game g WHERE g.season.id = :id")
+	public List<Game> findGamesBySeasonId(@Param("id") Long id);
 }
--- src/main/java/life/pifrans/repositories/ScoreRepository.java
+++ src/main/java/life/pifrans/repositories/ScoreRepository.java
@@ -1,5 +1,7 @@
 package life.pifrans.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +14,10 @@ public interface ScoreRepository extends JpaRepository<Score, Long> {
 
 	@Query("SELECT s FROM Score s WHERE s.points = (SELECT MIN(s.points) FROM Score s WHERE s.player.id = :id)")
 	public Score findMinRecord(@Param("id") Long id);
-	
+
 	@Query("SELECT s FROM Score s WHERE s.points = (SELECT MAX(s.points) FROM Score s WHERE s.player.id = :id)")
 	public Score findMaxRecord(@Param("id") Long id);
+
+	@Query("SELECT s FROM Score s WHERE s.game.id = :id")
+	public List<Score> findScoresByGamesId(@Param("id") Long id);
 }
